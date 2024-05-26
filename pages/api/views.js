3bdc8bd684dbb7d6952664ae1d813e2c09765607
shihaanws <shihaanws@gmail.com>
2024-05-26 23:03:35 +0530

    redis env fix

--- pages/api/views.js
+++ pages/api/views.js
@@ -1,48 +1,51 @@
 import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
+  token: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN,
+});
 export const runtime = "edge";
 
 export default async function views(req) {
-    if (req.method !== "POST") {
-        return new NextResponse("Request should be POST", { status: 405 });
-    }
-    if (req.headers.get("Content-Type") !== "application/json") {
-        return new NextResponse("Content must be JSON", { status: 400 });
-    }
+  if (req.method !== "POST") {
+    return new NextResponse("Request should be POST", { status: 405 });
+  }
+  if (req.headers.get("Content-Type") !== "application/json") {
+    return new NextResponse("Content must be JSON", { status: 400 });
+  }
 
-    const body = await req.json();
-    let slug = undefined;
-    if ("slug" in body) {
-        slug = body.slug;
-    }
+  const body = await req.json();
+  let slug = undefined;
+  if ("slug" in body) {
+    slug = body.slug;
+  }
 
-    if (!slug) {
-        return new NextResponse("Slug not found", { status: 400 });
-    }
-    
-    const ip = req.ip;
-    if (ip) {
-        // Hash the IP
-        const buf = await crypto.subtle.digest(
-            "SHA-256",
-            new TextEncoder().encode(ip)
-        );
-        const hash = Array.from(new Uint8Array(buf))
-            .map(b => b.toString(16).padStart(2, "0"))
-            .join("");
+  if (!slug) {
+    return new NextResponse("Slug not found", { status: 400 });
+  }
 
-        const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
-            nx: true,
-            ex: 24 * 60 * 60
-        });
-        if (!isNew) {
-            return new NextResponse(null, { status: 200 });
-        }
-    }
-    
-    await redis.incr(["pageviews", "page", slug].join(":"));
+  const ip = req.ip;
+  if (ip) {
+    // Hash the IP
+    const buf = await crypto.subtle.digest(
+      "SHA-256",
+      new TextEncoder().encode(ip)
+    );
+    const hash = Array.from(new Uint8Array(buf))
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
 
-    return new NextResponse(null, { status: 200 });
+    const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
+      nx: true,
+      ex: 24 * 60 * 60,
+    });
+    if (!isNew) {
+      return new NextResponse(null, { status: 200 });
+    }
+  }
+
+  await redis.incr(["pageviews", "page", slug].join(":"));
+
+  return new NextResponse(null, { status: 200 });
 }
